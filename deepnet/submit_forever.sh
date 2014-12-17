11a0b5461561b39693b33a3fea61e4f0a0496cbe
“Subhodeep <subhodeeep.moitra@gmail.com>
2014-12-17 09:08:05 -0500

    Minor typo

--- deepnet/submit_forever.sh
+++ deepnet/submit_forever.sh
@@ -2,7 +2,7 @@
 
 while [ 1 ] ; do
     python manage_experiments.py --pull_finished
-    python manage_experiment.py --update_expalloc
+    python manage_experiments.py --update_expalloc
     fab aws_idle_run_exp:deepnet 
 done
 
