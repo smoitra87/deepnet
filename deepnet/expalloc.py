ced06ad48bf8be55b5dca4032a62b1d8f469140d
“Subhodeep <subhodeeep.moitra@gmail.com>
2015-04-20 22:10:11 -0400

    Adding PCD jobs

--- deepnet/expalloc.py
+++ deepnet/expalloc.py
@@ -1,7 +1,7 @@
 
-exp_to_args = {'exp18039': '--initial_momentum 0.5 --final_momentum 0.9 --model_dir experiments/exp18039/dbm_models --rep_dir experiments/exp18039/dbm_reps --l2_decay 0.01 --base_epsilon 0.1 --model dbm --hidden1_width 1000 --hidden2_width 500 --steps 100000 --batchsize 500 --input_width 54 --data_dir datasets/PF13833'}
+exp_to_args = {'exp18059': '--initial_momentum 0.5 --final_momentum 0.9 --model_dir experiments/exp18059/dbm_models --rep_dir experiments/exp18059/dbm_reps --l2_decay 0.01 --base_epsilon 0.1 --model rbm --hidden1_width 1000 --bernoulli2_hidden1_width 10 --steps 100000 --batchsize 500 --input_width 54 --data_dir datasets/PF13833', 'exp18058': '--initial_momentum 0.5 --final_momentum 0.9 --model_dir experiments/exp18058/dbm_models --rep_dir experiments/exp18058/dbm_reps --l2_decay 0.01 --base_epsilon 0.1 --model rbm --hidden1_width 100 --bernoulli2_hidden1_width 10 --steps 100000 --batchsize 500 --input_width 54 --data_dir datasets/PF13833'}
 
 #################################33333
 
-name_to_exp = {'deepnet1': ['exp18039']}
+name_to_exp = {'deepnet1': ['exp18058'], 'deepnet2': ['exp18059']}
         
--- deepnet/experiments/run_in_parallel.sh
+++ deepnet/experiments/run_in_parallel.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-( cd exp18020 ; ./runall.sh ; cd ..  ;  cd exp18024 ; ./runall.sh ; cd ..  ;  cd exp18028 ; ./runall.sh ; cd ..  ;  cd exp18032 ; ./runall.sh ; cd ..  ;  cd exp18036 ; ./runall.sh ; cd .. ) & 
+( cd exp18040 ; ./runall.sh ; cd ..  ;  cd exp18044 ; ./runall.sh ; cd ..  ;  cd exp18048 ; ./runall.sh ; cd ..  ;  cd exp18052 ; ./runall.sh ; cd ..  ;  cd exp18056 ; ./runall.sh ; cd .. ) & 
 
-( cd exp18021 ; ./runall.sh ; cd ..  ;  cd exp18025 ; ./runall.sh ; cd ..  ;  cd exp18029 ; ./runall.sh ; cd ..  ;  cd exp18033 ; ./runall.sh ; cd ..  ;  cd exp18037 ; ./runall.sh ; cd .. ) & 
+( cd exp18041 ; ./runall.sh ; cd ..  ;  cd exp18045 ; ./runall.sh ; cd ..  ;  cd exp18049 ; ./runall.sh ; cd ..  ;  cd exp18053 ; ./runall.sh ; cd ..  ;  cd exp18057 ; ./runall.sh ; cd .. ) & 
 
-( cd exp18022 ; ./runall.sh ; cd ..  ;  cd exp18026 ; ./runall.sh ; cd ..  ;  cd exp18030 ; ./runall.sh ; cd ..  ;  cd exp18034 ; ./runall.sh ; cd ..  ;  cd exp18038 ; ./runall.sh ; cd .. ) & 
+( cd exp18042 ; ./runall.sh ; cd ..  ;  cd exp18046 ; ./runall.sh ; cd ..  ;  cd exp18050 ; ./runall.sh ; cd ..  ;  cd exp18054 ; ./runall.sh ; cd ..  ;  cd exp18058 ; ./runall.sh ; cd .. ) & 
 
-( cd exp18023 ; ./runall.sh ; cd ..  ;  cd exp18027 ; ./runall.sh ; cd ..  ;  cd exp18031 ; ./runall.sh ; cd ..  ;  cd exp18035 ; ./runall.sh ; cd ..  ;  cd exp18039 ; ./runall.sh ; cd .. ) & 
+( cd exp18043 ; ./runall.sh ; cd ..  ;  cd exp18047 ; ./runall.sh ; cd ..  ;  cd exp18051 ; ./runall.sh ; cd ..  ;  cd exp18055 ; ./runall.sh ; cd ..  ;  cd exp18059 ; ./runall.sh ; cd .. ) & 
